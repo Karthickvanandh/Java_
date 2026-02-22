86dee7e47efdb8c71e99138686d0a3ef3f0ede0a
Karthickvanandh <karthickvanandh@gmail.com>
2026-02-22 19:21:11 +0530

    feat: Element Occurance pblm

--- Array/elementOccurance.java
+++ Array/elementOccurance.java
@@ -5,11 +5,17 @@ import java.util.*;
 public class elementOccurance {
 
     public static void findOccu(int[] arr, int[] arr1){
+        int visited = -1;
         for(int i=0;i<arr.length;i++){
+            int count = 1;
             for(int j=i+1;j<arr.length;j++){
                 if(arr[i] == arr[j]){
-                    
+                    count++;
+                    arr1[j] = visited;
                 }
+            }
+            if(arr1[i] != -1){
+                arr1[i] = count;
             }
         }
     }
@@ -22,5 +28,12 @@ public class elementOccurance {
         }
         int[] arr1 = new int[n];
         findOccu(arr, arr1);
+
+        System.out.println("Elements" + " " + "|" + " " + "Occurance");
+        for(int i=0;i<n;i++){
+            if(arr1[i] != -1){
+                System.out.println(" " + arr[i] + " " + "|" + " " + arr1[i]);
+            }
+        }
     }
 }
