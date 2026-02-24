c2d4cd42965e00222c0bcd35fadfa7e90bfb2e69
Karthickvanandh <karthickvanandh@gmail.com>
2026-02-24 16:00:32 +0530

    feat: Sorting in Desc

--- Array/SortElement2.java
+++ Array/SortElement2.java
@@ -1,32 +1,31 @@
 package Array;
 import java.util.*;
 
-//[10, 2, 23, 13, 4, 53]
-public class SortElement {
+public class SortElement2 {
 
-    public static void SortAsc(int[] arr){
+    public static void SortDesc(int[] arr){
         for(int i=0;i<arr.length;i++){
             for(int j=i+1;j<arr.length;j++){
-                if(arr[i] > arr[j]){
-                    int temp = arr[j];
-                    arr[j] = arr[i];
-                    arr[i] = temp;
+                if(arr[i] < arr[j]){
+                    int temp = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = temp;
                 }
             }
         }
     }
     public static void main(String args[]){
-        Scanner in = new Scanner(System.in);
+       Scanner in = new Scanner(System.in);
         int n = in.nextInt();
         int[] arr = new int[n];
         System.out.print("Enter the value: ");
         for(int i=0;i<n;i++){
             arr[i] = in.nextInt();
         }
-        SortAsc(arr);
+        SortDesc(arr);
         System.out.print("After sorting: ");
         for(int i=0;i<n;i++){
             System.out.print(arr[i] + " ");
-        }
+        } 
     }
 }
