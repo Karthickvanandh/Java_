02d3c846f763f7a039a3562479030907b1d4e2f1
Karthickvanandh <karthickvanandh@gmail.com>
2026-03-20 01:44:25 +0530

    Fix: Checking all the file

--- OOPS/BasicStructureOops.java
+++ OOPS/BasicStructureOops.java
@@ -5,21 +5,22 @@ class Student{
     int id;
     String name;
 
-    void getDetails(int id, String name){
+    void getDetails(){
         System.out.print("Student rollno: " + id);
         System.out.print("Student name: " + name);
     }
 
-    void setDetails(){
-        id = 102;
-        name = "DEF";
+    void setDetails(int id,String name){
+        id = id;
+        name = name;
     }
 }
 public class BasicStructureOops {
     public static void main(String args[]){
         Student s1 = new Student();
-        s1.getDetails(101, "ABC");
-        s1.setDetails();
+        s1.setDetails(101, "ABC");
+        s1.getDetails();
+        
         System.out.print("After set id: " + s1.id);
         System.out.print("After set name: " + s1.name);
     }
