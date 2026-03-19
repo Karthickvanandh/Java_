98e4f191f40b4bc095c17d97c441879f39550c33
Karthickvanandh <karthickvanandh@gmail.com>
2026-03-20 03:43:02 +0530

    Fix: Added random to generate random value for matrix

--- Array/addMatrix.java
+++ Array/addMatrix.java
@@ -2,7 +2,25 @@ package Array;
 import java.util.*;
 
 public class addMatrix {
-    public static int[][] add(int[][] arr1, int[][] arr2, int n, int m){
+
+    public static void displayMatrix(int[][] arr1, int[][] arr2, int n, int m){  //printing matrix value
+        System.out.println("Printing martix 1 values");
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                System.out.print(arr1[i][j] + " ");
+            }
+            System.out.println();
+        }
+        System.out.println("Printing matrix 2 values");
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                System.out.print(arr2[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static int[][] add(int[][] arr1, int[][] arr2, int n, int m){   // Adding two matrix
         int[][] res = new int[n][m];
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
@@ -13,6 +31,7 @@ public class addMatrix {
     }
     public static void main(String args[]){
         Scanner in = new Scanner(System.in);
+        Random rand = new Random();  //Random method
         System.out.println("Enter the number of rows: ");
         int n = in.nextInt();
         System.out.println("Enter the number of column: ");
@@ -21,16 +40,19 @@ public class addMatrix {
         System.out.println("Enter the value for matirx 1: ");
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
-                arr1[i][j] = in.nextInt();
+                arr1[i][j] = (rand.nextInt(100) +1);   //Using random to generate the value for matrix
             }
         }
         int[][] arr2 = new int[n][m];
         System.out.println("Enter the value for martix 2: ");
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
-                arr2[i][j] = in.nextInt();
+                arr2[i][j] = (rand.nextInt(100) +1);
             }
         }
+
+        displayMatrix(arr1, arr2, n, m);
+
         int ans[][] = add(arr1, arr2, n, m);
         System.out.println("After the addition");
         for(int i=0;i<n;i++){
