7c4fc28c47fee5a6be14e6007588fc9ddbd353d3
Karthickvanandh <karthickvanandh@gmail.com>
2026-05-18 03:24:25 +0530

    Feat: Added billing class

--- OOPS/RetailStoreEx1.java
+++ OOPS/RetailStoreEx1.java
@@ -25,6 +25,55 @@ class Product{
         }
 }
 
+class Customer{
+    String name;
+    long phone;
+    static int totalCustomers;
+
+    Customer(String name, long phone){
+        this.name = name;
+        this.phone = phone;
+        totalCustomers++;
+    }
+
+    void showCustomer(){
+        System.out.println("Customer name: " +name );
+        System.out.println("Phone: " +phone);
+    }
+
+    static void showTotalCustomers(){
+        System.out.println("Total Customers: " +totalCustomers);
+
+    }
+}
+
+
+class Bill{
+    Customer customer;
+    double totalAmount;
+    static int totalBill;
+
+    Bill(Customer customer){
+        this.customer = customer;
+        totalAmount = 0;
+        totalBill++;
+    }
+    void addItem(Product p, int qty){
+        totalAmount += (p.price * qty);
+        System.out.println("Added: " +p.name);
+    }   
+
+
+    void printBills(){
+        this.customer.showCustomer();   
+        System.out.println("Total Amount: "+totalAmount);
+    }
+
+    static void showTotalBills(){
+        System.out.println("Total Bills: "+totalBill);
+    }
+}
+
 public class RetailStoreEx1 {
     public static void main(String args[]){
         Product p1 = new Product("candy", 10, 2);
@@ -32,7 +81,14 @@ public class RetailStoreEx1 {
         p1.showProduct();
         p2.showProduct();
         Product.showTotalProduct();
-
-    }
-    
+        Customer c1 = new Customer("AAA",1234567890);
+        Customer c2 = new Customer("BBB",1345678901);
+        c1.showCustomer();
+        c2.showCustomer();
+        Customer.showTotalCustomers();
+        Bill b1 = new Bill(c1);
+        b1.addItem(p1, 3);
+        b1.printBills();
+        Bill.showTotalBills();
+    }    
 }
