44ce25ec2ae634f3f341197b01c17e898163b204
Karthickvanandh <karthickvanandh@gmail.com>
2026-05-30 19:04:28 +0530

    Feat: Added login

--- OOPS/Bank.java
+++ OOPS/Bank.java
@@ -2,22 +2,27 @@ package OOPS;
 
 class BankAccount{
     String customerName;
-    long accountNumber;
+    private long accountNumber;
+    private String password;
     long phoneNumber;
     private double Balance;
 
-    BankAccount(String customerName, long accountNumber, long phoneNumber){
+    BankAccount(String customerName, long accountNumber, String password, long phoneNumber){
         this.customerName = customerName;
         this.accountNumber = accountNumber;
+        this.password = password;
         this.phoneNumber = phoneNumber;
     }
 
-    // boolean login(){
-
-
-    // }
-
-
+    boolean login(long accountNo, String pass){
+        if(this.accountNumber == accountNo && this.password.equals(pass)){
+            return true;
+        }else{
+            return false;
+        }
+    }
+    
+    
     void setBalance(double Balance){
         if(Balance < 0){
             System.out.println("Invalid Balance");
@@ -36,11 +41,12 @@ class SavingsAccount extends BankAccount{
 
     static String accountType = "Saving Account";
 
-    SavingsAccount(String customerName, long accountNumber, long phoneNumber){
-        super(customerName, accountNumber, phoneNumber);
-    }    
+    SavingsAccount(String customerName, long accountNumber, String password, long phoneNumber){
+        super(customerName, accountNumber, password, phoneNumber);
+    }
+    
 
-    void Deposite(double amount){
+    void deposite(double amount){
         if(getBalance() < 0){
             System.out.println("Invalid Balance");
         }
@@ -50,7 +56,7 @@ class SavingsAccount extends BankAccount{
         }
     }
 
-    void Withdraw(double amount){
+    void withdraw(double amount){
         if(getBalance() < 0){
             System.out.println("Invalid Balance");
         }
@@ -67,18 +73,19 @@ class SavingsAccount extends BankAccount{
 
 public class Bank {
     public static void main(String args[]){
-        SavingsAccount s1 = new SavingsAccount("AAA", 12332131, 1234567890);
-        System.out.println("Customer Details: " + s1.customerName);
-        System.out.println("accountNumber: " + s1.accountNumber);
-        System.out.println("phoneNumber: " + s1.phoneNumber);
-        System.out.println("Total Balance: " + s1.getBalance());
-        s1.setBalance(1000);
-        System.out.println("Total Balance: " + s1.getBalance());
-        s1.displayBalance();
-        s1.Deposite(20000);
-        s1.displayBalance();
-        s1.Withdraw(5000);
-        s1.displayBalance();
+        SavingsAccount s1 = new SavingsAccount("AAA", 12332131, "1234", 1234567890);
+        if(s1.login(12332131, "1234")){
+            s1.setBalance(1000);
+            System.out.println("Total Balance: " + s1.getBalance());
+            s1.displayBalance();
+            s1.deposite(20000);
+            s1.displayBalance();
+            s1.withdraw(5000);
+            s1.displayBalance();
+        }else{
+            System.out.println("Invalid credentials!");     
+        }
+        
 
     }
     
