9f275fa3c2137f731a3abcbe85c488390807640f
Karthickvanandh <karthickvanandh@gmail.com>
2026-07-16 21:17:39 +0530

    Feat: Testing OOP 2 - insertin

--- DB/BasicDBConnection.java
+++ DB/BasicDBConnection.java
@@ -1,5 +1,6 @@
 package DB;
 import java.sql.*;
+import java.util.*;
 
 public class BasicDBConnection {
     public static void main(String[] args) throws SQLException{
@@ -7,16 +8,43 @@ public class BasicDBConnection {
         
         Connection conn = DriverManager.getConnection("jdbc:postgresql://localhost:5432/employee", "postgres" , "admin");
 
-        Statement st = conn.createStatement();
+        PreparedStatement st = conn.prepareStatement("SELECT * FROM employee");
 
-        ResultSet rs = st.executeQuery("SELECT * FROM employee");
-
+        ResultSet rs = st.executeQuery();
+        //fetch all data
         while(rs.next()){
             int id = rs.getInt("id");
             String name = rs.getString("name");
             double salary = rs.getDouble("salary");
             System.out.println(id + " " + name + " " + salary);
         }
+
+        //fetch single data from db
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter id: ");
+        int id = in.nextInt();      
+        System.out.println("Enter name: ");
+        String name = in.next();
+        System.out.println("Enter salary: ");
+        double salary = in.nextDouble();
+
+        
+
+        PreparedStatement insertst = conn.prepareStatement("INSERT INTO employee(id, name, salary) VALUES(?, ?, ?)");
+        insertst.setInt(1, id);
+        insertst.setString(2, name);
+        insertst.setDouble(3, salary);
+        
+
+        st = conn.prepareStatement("SELECT * FROM employee");
+
+        while(rs.next()){
+            int id1 = rs.getInt("id");
+            String name1 = rs.getString("name");
+            double salary1 = rs.getDouble("salary");
+            System.out.println(id1 + " " + name1 + " " + salary1);
+        }
+
         rs.close();
         st.close();
         conn.close();
